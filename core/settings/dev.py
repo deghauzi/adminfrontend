eb1292082a7924111ff38960e616faa2c2dd5586
hudeh <hudeh30@gmail.com>
2021-12-01 23:22:52 +0100

    refactor code structure

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -11,7 +11,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",  
-        "NAME": "deghauzi_app",
+        "NAME": "app",
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "localhost",
